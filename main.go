04d918d483ca636875759e72cdaacf6c8d44514f
dkeza <dkezic@yahoo.com>
2018-10-27 20:12:10 +0200

    Adding 404 page

--- main.go
+++ main.go
@@ -8,9 +8,12 @@ import (
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome here</h1>")
+		fmt.Fprint(w, "<h1>Welcome here :)</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "<p>Contact page</p>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>Nothing here</h1>")
 	}
 }
 
