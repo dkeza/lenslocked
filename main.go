6f0b441a8b841d18d96a55736c708c220a0eb523
dkeza <dkezic@yahoo.com>
2018-10-28 08:16:16 +0100

    Use Gorilla mux

--- main.go
+++ main.go
@@ -3,21 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome here :)</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "<p>Contact page</p>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>Nothing here</h1>")
-	}
+	fmt.Fprint(w, "<h1>Welcome here :)</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<p>Contact page</p>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 }
