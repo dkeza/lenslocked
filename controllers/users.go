dd59287c5fb6ce38a4cc2e92c0f406755d712478
dkeza <dkezic@yahoo.com>
2018-11-09 23:31:35 +0100

    Parse from data

--- controllers/users.go
+++ controllers/users.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"lenslocked/views"
 	"net/http"
+
+	"github.com/gorilla/schema"
 )
 
 func NewUsers() *Users {
@@ -22,8 +24,23 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	u.NewView.Render(w, nil)
 }
 
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 // Create new user
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "User created")
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+
+	dec := schema.NewDecoder()
+	var form SignupForm
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintln(w, form)
 }
