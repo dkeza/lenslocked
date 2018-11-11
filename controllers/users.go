e61e1abda20133b43d8437563401b10c362af562
dkeza <dkezic@yahoo.com>
2018-11-11 11:04:53 +0100

    Form parsing helper

--- controllers/users.go
+++ controllers/users.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"lenslocked/views"
 	"net/http"
-
-	"github.com/gorilla/schema"
 )
 
 func NewUsers() *Users {
@@ -32,13 +30,9 @@ type SignupForm struct {
 // Create new user
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		panic(err)
-	}
-
-	dec := schema.NewDecoder()
 	var form SignupForm
-	if err := dec.Decode(&form, r.PostForm); err != nil {
+
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
 
