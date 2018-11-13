071618cadc21c79aa85660a495d4650408ea38b2
dkeza <dkezic@yahoo.com>
2018-11-13 20:40:21 +0100

    Database structure

---
+++ db/database.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	email TEXT NOT NULL
+);
+
+CREATE TABLE orders (
+	id SERIAL PRIMARY KEY,
+	user_id INT NOT NULL,
+	amount INT,
+	description TEXT NOT NULL
+);
