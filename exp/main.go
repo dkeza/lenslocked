b81e84616c7ab6d50be11ef781965bb50a3a7a5b
dkeza <dkezic@yahoo.com>
2018-11-19 20:34:31 +0100

    Create User service

---
+++ exp/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"lenslocked/models"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "1234"
+	dbname   = "lenslocked_dev"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	us, err := models.NewUserService(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
+
+	user, err := us.ByID(111)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(user)
+
+}
