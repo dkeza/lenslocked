ef35ae4e16b629da9557fe9fe50892ef330f47e7
dkeza <dkezic@yahoo.com>
2019-01-11 18:25:29 +0100

    Show image names list

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -190,8 +190,12 @@ func (g *Galleries) ImageUpload(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-
-	fmt.Fprintln(w, "Uploaded!")
+	url, err := g.r.Get("edit_gallery").URL("id", fmt.Sprintf("%v", gallery.ID))
+	if err != nil {
+		http.Redirect(w, r, "/galleries", http.StatusFound)
+		return
+	}
+	http.Redirect(w, r, url.Path, http.StatusFound)
 }
 
 // POST /galleries/:id/delete
@@ -234,5 +238,7 @@ func (g *Galleries) galleryByID(w http.ResponseWriter, r *http.Request) (*models
 		}
 		return nil, err
 	}
+	images, _ := g.is.ByGalleryID(gallery.ID)
+	gallery.Images = images
 	return gallery, nil
 }
--- models/galleries.go
+++ models/galleries.go
@@ -7,8 +7,9 @@ import (
 // Gallery is image container
 type Gallery struct {
 	gorm.Model
-	UserID uint   `gorm:"not_null;index"`
-	Title  string `gorm:"not_null"`
+	UserID uint     `gorm:"not_null;index"`
+	Title  string   `gorm:"not_null"`
+	Images []string `gorm:"-"`
 }
 
 type GalleryService interface {
